13a4cdf715c745d0959769918732b2182160e5bc
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-01 03:05:56 -0400

    Added navigation
    Added an amazing new navigation bar

--- js/blockd.js
+++ js/blockd.js
@@ -1,3 +1,38 @@
 /**
  * Created by Jackson on 7/31/16.
  */
+
+$(document).ready(function () {
+    var transparent = true;
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    var mainbottom = $('.top').offset().top + $('.top').height() - 250;
+
+    // on scroll,
+    $(window).on('scroll',function(){
+
+        // we round here to reduce a little workload
+        var stop = Math.round($(window).scrollTop());
+
+        if (stop > mainbottom) {
+            $('.navigation').addClass('nav-fixed');
+            $('.brand').removeClass('hidden');
+            $('.forums').removeClass('.col-md-offset-1');
+        } else {
+            $('.navigation').removeClass('nav-fixed');
+            $('.brand').addClass('hidden');
+            $('.forums').addClass('.col-md-offset-1');
+        }
+
+    });
+});
